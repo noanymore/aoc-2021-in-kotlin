d6acfe6de04b1626e4a573ec9f87ab230ba61e50
songqicong <sqc95111@163.com>
2023-01-10 18:38:25 +0800

    day01 commited

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,6 +1,15 @@
 fun main() {
+    val sum : MutableList<Int> = mutableListOf()
     fun part1(input: List<String>): Int {
-        return input.size
+        var singSum : Int = 0
+        for(a in input.indices){
+            singSum = if (input[a]=="") singSum else singSum+input[a].toInt()
+            if (a>input.size-2||input[a+1]==""){
+                sum.add(singSum)
+                singSum = 0
+            }
+        }
+        return sum.max();
     }
 
     fun part2(input: List<String>): Int {
@@ -9,9 +18,11 @@ fun main() {
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    println(part1(testInput))
+    check(part1(testInput) == 24000)
 
     val input = readInput("Day01")
     part1(input).println()
     part2(input).println()
 }
+
