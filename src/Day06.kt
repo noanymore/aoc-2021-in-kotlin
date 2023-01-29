4530659a7d31b400d5bfa294fa4c162f6506062d
songqicong <sqc95111@163.com>
2023-01-29 10:45:47 +0800

    day06 update

--- src/Day06.kt
+++ src/Day06.kt
@@ -1,22 +1,29 @@
 fun main() {
 
     fun part1(input: String): Int {
-        var map = hashMapOf<Char,Int>()
         var start = 0
-        var end = start+1
+        var end = 0
         var nextStart = 0
-        while (end-start<14){
-            map.put(input[start],start)
-            if(map.contains(input[end])){
-                val bound = map[input[end]]
-                map.remove(input[end])
-                map.put(input[end+1],end+1)
-                nextStart = if( map.isEmpty() ) start+1 else map.values.filter { it > bound!! }.min()
+        var hasNext = false
+        val cache = hashMapOf<Char,Int>()
+        while (end - start < 14){
+            if (end == start) end++
+            if (input[start]!=input[end]){
+                cache[input[end]] = end
+                if(!hasNext) {
+                    nextStart = end
+                    hasNext = true
+                }
+            }
+            if(cache.contains(input[end])){
+                val index = cache[input[end]]
+                if(index == end-1){
+                    nextStart = end+1
+                }else{
+                    nextStart = cache.values.filter { it > index!! }.min()
+                }
                 start = nextStart
-                end = nextStart
-                map.clear()
-            }else{
-                map[input[end]] = end
+                hasNext = false
             }
             end++
         }
